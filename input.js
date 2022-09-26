a96cb54b3364757d0610b652bfc938c14355e54f
OhBeomho <tyler0322@naver.com>
2022-09-26 21:10:39 +0900

    a

--- input.js
+++ input.js
@@ -1,20 +1,4 @@
 document.querySelectorAll('.input').forEach((e) => {
-	e.querySelector('input').addEventListener('focus', () => {
-		e.classList.add('active');
-
-		if (!e.classList.contains('error')) {
-			e.querySelector('.placeholder').classList.add('activecolor');
-		}
-
-		e.querySelector('.placeholder').classList.add('active');
-	});
-
-	e.querySelector('input').addEventListener('focusout', () => {
-		if (!e.querySelector('input').value) {
-			e.querySelector('.placeholder').classList.remove('active');
-		}
-
-		e.classList.remove('active');
-		e.querySelector('.placeholder').classList.remove('activecolor');
-	});
-});
+	e.querySelector('input').addEventListener('focus', () => e.classList.add('active'))
+	e.querySelector('input').addEventListener('focusout', () => e.classList.remove('active'))
+})
