a31cfd17a0e244ed32b40236ec95b08707b76fa1
OhBeomho <tyler0322@naver.com>
2022-10-23 09:47:04 +0900

    removed input script

--- js/input.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.input').forEach((e) => {
-	e.querySelector('input').addEventListener('focus', () => e.classList.add('active'))
-	e.querySelector('input').addEventListener('focusout', () => e.classList.remove('active'))
-})
